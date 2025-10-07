b26c17498f6c8dd95b21a0493c809681a08f88e2
burritosoftware <37681398+burritosoftware@users.noreply.github.com>
2025-10-07 00:03:34 -0700

    Add VTA extension

---
+++ extensions/vta.py
@@ -0,0 +1,25 @@
+import hikari
+import lightbulb
+import sys
+
+sys.path.append("..")
+
+from helpers import (
+    sapi
+)
+
+loader = lightbulb.Loader()
+
+@loader.command()
+class Samantha(
+    lightbulb.SlashCommand,
+    name="samantha",
+    description="Speak using Samantha's voice (VTA)",
+):
+    text = lightbulb.string("text", 'What you want Samantha to say')
+
+    @lightbulb.invoke
+    async def invoke(self, ctx: lightbulb.Context) -> None:
+        await ctx.defer()
+        wav = await sapi.synthesize(text=self.text, voice="VEX_Samantha [English (United States)]")
+        await ctx.respond(attachments=[hikari.Bytes(wav, "samantha.wav")])
